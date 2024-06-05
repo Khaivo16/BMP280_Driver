86a408334baed3eed4bba28b32cd4f0dd72fa6ed
Khaivo16 <khainbk197@gmail.com>
2024-06-05 12:18:48 +0700

    version 2

--- Test_code/Test_modeI2C.c
+++ Test_code/Test_modeI2C.c
@@ -10,7 +10,7 @@ float PER;
 int main() {
     wiringPiSetup();
     int fd;
-    fd = BMP_mode(SPI_mode);
+    fd = BMP_mode(I2C_mode);
     BMP_config_0xF5(fd,BMP280_STANDBY_TIME_1000_MS,BMP280_FILTER_COEFF_OFF);
     BMP_ctrl_meas_0xF4(fd,BMP280_OVERSAMP_1X,BMP280_OVERSAMP_1X,BMP280_NORMAL_MODE);
     // BMP_mode_active(fd,WEATHER_MONITORING); 
